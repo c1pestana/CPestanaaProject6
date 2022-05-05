a27d0260594de40b4cc50e391584aaa437a90a8a
C1PESTANA <c1pestana@student.bridgew.edu>
2022-05-05 14:26:59 -0400

    getting there!

--- Comp151Window.py
+++ Comp151Window.py
@@ -8,24 +8,6 @@ class Comp151Window(arcade.Window):
     def setup(self):
         """ Set up the game here. Call this function to restart the game. """
         pass
-    #
-    # def update(self, delta_time: float):
-    #     self.firstlog.change_x = +2
-    #     self.secondlog.change_x = -2
-    #     self.thirdlog.change_x = +4
-    #     self.fourthlog.change_x = -3
-    #     self.firstcar.change_x = +2
-    #     self.secondcar.change_x = -2
-    #     self.thirdcar.change_x = +4
-    #     self.fourthcar.change_x = -3
-    #     self.firstlog.center_x += self.firstcar.change()
-    #     self.secondlog.center_x += self.secondcar.change()
-    #     self.thirdlog.center_x += self.thirdcar.change()
-    #     self.fourthlog.center_x += self.fourthcar.change()
-    #     self.firstcar.center_x += self.firstlog.change()
-    #     self.secondcar.center_x += self.secondlog.change()
-    #     self.thirdcar.center_x += self.thirdlog.change()
-    #     self.fourthcar.center_x += self.fourthlog.change()
 
     def on_draw(self):
         """ Render the screen. """
--- main.py
+++ main.py
@@ -14,15 +14,32 @@ def main():
     window.startpanel = arcade.create_rectangle(400, 25, window.width, 50, arcade.color.FOREST_GREEN)
     window.safespot = arcade.create_rectangle(400, 400, window.width, 50, arcade.color.BLACK)
     window.frog = arcade.Sprite("frog.png")
-    window.firstcar = arcade.create_rectangle(25, 125, 50, 50, arcade.color.PURPLE)
-    window.secondcar = arcade.create_rectangle(775, 175, 50, 50, arcade.color.PURPLE)
-    window.thirdcar = arcade.create_rectangle(25, 250, 50, 50, arcade.color.PURPLE)
-    window.fourthcar = arcade.create_rectangle(775, 325, 50, 50, arcade.color.PURPLE)
-
-    window.firstlog = arcade.create_rectangle(25, 500, 50, 50, arcade.color.DARK_BROWN)
-    window.secondlog = arcade.create_rectangle(775, 575, 50, 50, arcade.color.DARK_BROWN)
-    window.thirdlog = arcade.create_rectangle(25, 625, 50, 50, arcade.color.DARK_BROWN)
-    window.fourthlog = arcade.create_rectangle(775, 700, 50, 50, arcade.color.DARK_BROWN)
+    window.firstcar = arcade.Sprite("mushroom.png")
+    window.secondcar = arcade.Sprite("mushroom.png")
+    window.thirdcar = arcade.Sprite("mushroom.png")
+    window.fourthcar = arcade.Sprite("mushroom.png")
+    window.firstlog = arcade.Sprite("mushroom.png")
+    window.secondlog = arcade.Sprite("mushroom.png")
+    window.thirdlog = arcade.Sprite("mushroom.png")
+    window.fourthlog = arcade.Sprite("mushroom.png")
+    window.firstcar.center_y = 50
+    window.secondcar.center_y = 100
+    window.thirdcar.center_y = 150
+    window.fourthcar.center_y = 200
+    window.firstlog.center_y = 300
+    window.secondlog.center_y = 350
+    window.thirdlog.center_y = 400
+    window.fourthlog.center_y = 450
+    
+    window.firstcar.center_x = 0
+    window.secondcar.center_x = 0
+    window.thirdcar.center_x = 0
+    window.fourthcar.center_x = 0
+    window.firstlog.center_x = 0
+    window.secondlog.center_x = 0
+    window.thirdlog.center_x = 0
+    window.fourthlog.center_x = 0
+    
     window.frog.center_x = 400
     window.frog.center_y = 25
     window.frog_dx = 0
@@ -36,6 +53,7 @@ def main():
 
 def comp151_draw(window):
     arcade.start_render()
+    update(window)
     update_frog_location(window)
     window.street.draw()
     window.endpanel.draw()
@@ -53,7 +71,7 @@ def comp151_draw(window):
     arcade.finish_render()
 
 
-def update(window, delta_time: float):
+def update(window):
     window.firstlog.change_x = +2
     window.secondlog.change_x = -2
     window.thirdlog.change_x = +4
@@ -61,15 +79,15 @@ def update(window, delta_time: float):
     window.firstcar.change_x = +2
     window.secondcar.change_x = -2
     window.thirdcar.change_x = +4
-    window.fourthcar.change_x_x = -3
-    window.firstlog.center_x += window.firstcar.change_x()
-    window.secondlog.center_x += window.secondcar.change_x()
-    window.thirdlog.center_x += window.thirdcar.change_x()
-    window.fourthlog.center_x += window.fourthcar.change_x()
-    window.firstcar.center_x += window.firstlog.change_x()
-    window.secondcar.center_x += window.secondlog.change_x()
-    window.thirdcar.center_x += window.thirdlog.change_x()
-    window.fourthcar.center_x += window.fourthlog.change_x()
+    window.fourthcar.change_x = -3
+    window.firstlog.center_x += window.firstcar.change_x
+    window.secondlog.center_x += window.secondcar.change_x
+    window.thirdlog.center_x += window.thirdcar.change_x
+    window.fourthlog.center_x += window.fourthcar.change_x
+    window.firstcar.center_x += window.firstlog.change_x
+    window.secondcar.center_x += window.secondlog.change_x
+    window.thirdcar.center_x += window.thirdlog.change_x
+    window.fourthcar.center_x += window.fourthlog.change_x
 
 def update_frog_location(window):
     if window.frog_dx != 0:
